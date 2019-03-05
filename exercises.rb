fcc71d2a342e633a11f8958b4cda16acd178b997
masha-b <mashabudryte@gmail.com>
2019-03-05 11:45:09 +0000

    first exercise added

---
+++ exercises.rb
@@ -0,0 +1,12 @@
+#imagined example
+#add_name("Bill")
+#=> "Name stored!"
+#the expectation
+#expect(add_name("Bill")).to eq "Name stored!"
+#the full test
+
+describe "the track_students method" do
+  it "tells us that the name was stored" do
+    expect(add_name("Bill")).to eq "Name stored!"
+  end
+end
